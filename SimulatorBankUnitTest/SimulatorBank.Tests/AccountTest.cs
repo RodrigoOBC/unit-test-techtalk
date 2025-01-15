b19c056d8924ca6ee3e5d2c06e71c91c38926975
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-01-15 13:46:37 -0300

    Add unit test for Account class to verify initial balance setting

---
+++ SimulatorBankUnitTest/SimulatorBank.Tests/AccountTest.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+using Xunit;
+using ModelsBank;
+using SimulatorBankUnitTest.ModelsBank.DBConnect;
+using System.Net;
+using System.Text.Json;
+namespace SimulatorBank.Tests;
+
+public class AccountTests
+{
+    [Fact]
+    public async Task Account_setInitalBalance_ShouldSetBalanceCorrectly()
+    {
+        // Arrange
+        var handlerMock = new Mock<HttpMessageHandler>();
+        var responseMessage = new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new StringContent(JsonSerializer.Serialize(new { Balance = 90 }))
+        };
+
+        handlerMock
+           .Protected()
+           .Setup<Task<HttpResponseMessage>>(
+              "SendAsync",
+              ItExpr.IsAny<HttpRequestMessage>(),
+              ItExpr.IsAny<CancellationToken>()
+           )
+           .ReturnsAsync(responseMessage);
+
+        var httpClient = new HttpClient(handlerMock.Object)
+        {
+            BaseAddress = new Uri("https://fd757376-23b1-4fa3-a13f-93fa8cf11485.mock.pstmn.io")
+        };
+
+        var clientMock = new Mock<Client>("teste", "123");
+        var account = new Account(clientMock.Object, 0, httpClient);
+
+        // Act
+        await account.setInitalBalance();
+
+        // Assert
+        Assert.Equal(90, account.Balance);
+    }
+}
